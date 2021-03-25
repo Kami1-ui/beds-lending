e6e8cd3014a7642dfc8e21a13b844e27bfef450b
Камиль Кузеров <hit.men061@gmail.com>
2021-03-25 17:24:13 +0400

    Mail fixed

--- js/script.js
+++ js/script.js
@@ -12,24 +12,4 @@ document.body.onload = function () {
     $('body').toggleClass('lock');
 }
 
-$(document).ready(function () {
-
-    //E-mail Ajax Send
-    $(".request__form").submit(function () { //Change
-        var th = $(this);
-        $.ajax({
-            type: "POST",
-            url: "mail.php", //Change
-            data: th.serialize()
-        }).done(function () {
-            alert("Thank you!");
-            setTimeout(function () {
-                // Done Functions
-                th.trigger("reset");
-            }, 1000);
-        });
-        return false;
-    });
-
-});
 
