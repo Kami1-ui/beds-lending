e593d5047d5c6022bb0dadc3a476a886f60a06fc
Камиль Кузеров <hit.men061@gmail.com>
2021-06-10 17:37:22 +0400

    Added section catalog

--- js/script.js
+++ js/script.js
@@ -13,3 +13,59 @@ document.body.onload = function () {
 }
 
 
+/* Слайдер*/
+$(document).ready(function () {
+
+    $('.carusel').slick({
+        arrows: true,
+        /*  prevArrow: '<button type="button" class="slick-prev"><i class="icon-left"></i></button>',*/
+        prevArrow: '<div>  <svg class="slick-prev"><use xlink: href = "img/icons/icons-sprite.svg#arrov-left" ></use></svg ></div>',
+        nextArrow: '<div><svg class="slick-next"><use xlink: href = "img/icons/icons-sprite.svg#arrov-right" ></use></svg > </div>',
+        //dots: true,
+        //adeptiveHight: true, flex-start
+        /*   slidesToShow: 6,
+          slidesToScroll: 5,
+          rows: 3, */
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+
+        //easing: 'ease',
+        //autoplay: true,
+        autoplaySpeed: 2500,
+
+        /*  responsive: [
+             {
+                 breakpoint: 1218,
+                 settings: {
+                     slidesToShow: 5,
+                     slidesToScroll: 4,
+                 }
+             },
+             {
+                 breakpoint: 1024,
+                 settings: {
+                     slidesToShow: 4,
+                     slidesToScroll: 3,
+                 }
+             },
+             {
+                 breakpoint: 768,
+                 settings: {
+                     slidesToShow: 3,
+                     slidesToScroll: 2,
+                 }
+             },
+             {
+                 breakpoint: 375,//576,
+                 settings: {
+                     slidesToShow: 2,
+                     slidesToScroll: 1,
+                     // dots: true,
+                     arrows: false,
+                 }
+             },
+         ] */
+    });
+});
--- mail.php
+++ mail.php
@@ -12,13 +12,13 @@ $phone = $_POST['Phone'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'bonlinelending@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'Cu61PVEU'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'kamil.kuz@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'cR/LiduFbRKS5=~'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('bonlinelending@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('dveriplusmebel@gmail.com');     // Кому будет уходить письмо 
+$mail->setFrom('kamil.kuz@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('hit.men061@gmail.com');     // Кому будет уходить письмо 
 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
